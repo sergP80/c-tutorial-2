d51cdfe3972a00a89bb0726b4b73fb1c6258a602
Sergii Puzyrov <sergii.puzyrov@gmail.com>
2025-09-24 13:33:28 +0300

    Добавьте файлы проекта.

---
+++ apps/lab01/lab01_1/src/app.c
@@ -0,0 +1,24 @@
+﻿#include "app.h"
+
+// Variant 20
+
+// sin^3.2(a-x)
+// -------------
+//   b + x^-1.7
+// a = 7.23, b = 3.43, x = 5.22
+
+int main()
+{
+	double a = 7.23, b = 3.43, x = 5.22;
+
+	double t1 = sin(a - x);
+
+	double t2 = pow(x, -1.7);
+
+	double result = pow(t1, 3.2) / (b + t2);
+
+	printf("sin^3.2(%.3f-%.3f)\n------------------------ = %.3f", a, x, result);
+	printf("\n%.3f + %.3f^-1.7", b, x);
+
+	return 0;
+}
