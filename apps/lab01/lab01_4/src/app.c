4b3df775497ba9e71335d602e87e143c1bc51b89
Sergii Puzyrov <sergii.puzyrov@gmail.com>
2025-12-06 15:46:05 +0200

    Functions, pointer to functions. Functions to process arrays. Simple calculator based on pointer to functions

---
+++ apps/lab01/lab01_4/src/app.c
@@ -0,0 +1,40 @@
+﻿#include "app.h"
+
+// Variant 20
+
+// sin^3.2(a-x)
+// -------------
+//   b + x^-1.7
+// a > x, x > 0
+
+#define PI 3.1415926
+
+#define SIN(a, x) sin((a) - (x))
+#define POW(x) pow((x), -1.7)
+
+#define EXPR(x, a, b) pow(SIN(a, x), 3.2)/((b) + POW(x))
+
+int main()
+{
+	double a, b, x;
+
+	printf("Enter a, b, x:");
+
+	int ret = scanf("%lf %lf %lf", &a, &b, &x);
+
+	if (ret != 3)
+	{
+		printf("Error! Cannot read 3 arguments but only %d\n", ret);
+		return -1;
+	}
+
+	printf("\n");
+
+	double result = EXPR(x, a, b);
+
+	printf("sin^3.2(%.3f-%.3f)\n------------------------ = %.3f", a, x, result);
+	
+	printf("\n%.3f + %.3f^-1.7", b, x);
+
+	return 0;
+}
---
+++ apps/lab04/demo/arrays/include/array_utils.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+void print_array_int(int a[], int sz);
+
+void search_min_max_int(int a[], int sz, int*, int*);
+
+void select_sort_int(int a[], int sz);
+
+void swap_int(int*, int*);
