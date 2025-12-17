b476db725efd86eae8d1ccacdd2ec699979f1a37
Сергей Пузырев <sergii.puzyrov@gmail.com>
2025-12-17 13:53:38 +0200

    Added geometry lib

---
+++ apps/lab06/demo/geometry-usage/src/app.c
@@ -0,0 +1,18 @@
+﻿#include "app.h"
+
+int main()
+{
+
+    POINT2D p1 = { 2.16, 3.22 };
+
+    POINT2D p2 = { -4.5, 7.55 };
+
+
+    double dist1 = distance(&p1, &p2);
+
+    printf("Distance: %.3f\n", dist1);
+
+    return 0;
+}
+
+
