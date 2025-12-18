601c5d28ef72da07c5e0bccb12764de13f45b83c
Sergii Puzyrov <sergii.puzyrov@gmail.com>
2025-12-18 15:51:10 +0200

    Fixed cross-platform library

--- libs/time-utils/src/time_utils.c
+++ libs/time-utils/src/time_utils.c
@@ -1,12 +1,32 @@
 ﻿#include "time-utils.h"
 #include <time.h>
 
+#if defined(_MSC_VER)
+#include <stdio.h>
+#endif
+
 long date_to_timestamp(char* date)
 {
     struct tm tm = {0};
 
-    strptime(date, DATE_TIME_FORMAT, &tm);
+#if defined(_MSC_VER)
+    int year, month, day;
 
+    if (sscanf(date, "%d-%d-%d", &year, &month, &day) != 3)
+        return -1;
+
+    tm.tm_year = year - 1900;
+    tm.tm_mon = month - 1;
+    tm.tm_mday = day;
+
+#else
+    if (strptime(date, DATE_TIME_FORMAT, &tm) == NULL)
+    {
+        return -1;
+    }
+        
+#endif
+    
     tm.tm_hour = 0;
     tm.tm_min  = 0;
     tm.tm_sec  = 0;
